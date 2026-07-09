18d949a068ed0a3b41ba4a54052224f6352eec8d
thushan-123 <thushanmadusanka456@gmail.com>
2026-07-09 20:23:57 +0530

    add middlewares cors

--- auth_service/main.py
+++ auth_service/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from router import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Auth Service"
+)
+
+origins = ["http://localhost:8001"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "UPDATE", "DELETE"],
+    allow_headers=["*"]
 )
 
 app.include_router(router)
